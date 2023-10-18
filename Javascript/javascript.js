7369d172ae75136d99e26718fd6248079d858fb7
RAJ123455666R3 <146659148+RAJ123455666R3@users.noreply.github.com>
2023-10-18 14:46:36 +0530

    Add files via upload

---
+++ Javascript/javascript.js
@@ -0,0 +1,20 @@
+const raj = prompt("hello what is your number");
+
+console.log("raj");
+
+
+if (raj === 5) {
+    console.log(" this is real raj");
+} else if (raj == 7) {
+    console.log("this is fake raj");
+} else if (raj === 4) {
+    console.log("this is not fake");
+} else {
+    console.log("this is real man");
+}
+
+
+if (raj !==23){
+
+ console.log("why not is not equal to twenty three");
+}
